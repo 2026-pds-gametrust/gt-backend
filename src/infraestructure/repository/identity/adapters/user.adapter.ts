449612da3bdf7cab06e26536bdddf2dbde0548bf
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-07 14:35:40 -0300

    feat(identity): add user adapter

---
+++ src/infraestructure/repository/identity/adapters/user.adapter.ts
@@ -0,0 +1,34 @@
+import { IUser } from '../../../../domain/identity/entity/interfaces/user.interface';
+import { IMUser } from '../../../db/mongo/models/user.model';
+
+export function dbToInternal(user: IMUser): IUser {
+  return {
+    id: user.id,
+    fullName: user.fullName,
+    email: user.email,
+    phone: user.phone,
+    cpf: user.cpf,
+    birthDate: user.birthDate,
+    verified: user.verified,
+    phoneVerified: user.phoneVerified,
+    status: user.status,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
+}
+
+export function internalToDb(
+  user: IUser,
+): Omit<IMUser, '_id' | 'createdAt' | 'updatedAt'> {
+  return {
+    id: user.id,
+    fullName: user.fullName,
+    email: user.email,
+    phone: user.phone,
+    cpf: user.cpf,
+    birthDate: user.birthDate,
+    verified: user.verified,
+    phoneVerified: user.phoneVerified,
+    status: user.status,
+  };
+}

524b44d0db9dc882e72e23a9f6cbfb4cc4b9711d
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-07 14:35:43 -0300

    chore(jest): update integration test setup

--- jest/setup-integration-tests.ts
+++ jest/setup-integration-tests.ts
@@ -1,5 +1,17 @@
 import { bootstrapTest } from '../src/__tests__/testUtils';
 import { Server } from '../src/domain/server/server';
+import { CategoryAttributeSchemaModel } from '../src/infraestructure/db/mongo/models/category-attribute-schema.model';
+import { CategoryModel } from '../src/infraestructure/db/mongo/models/category.model';
+import { FavoriteModel } from '../src/infraestructure/db/mongo/models/favorite.model';
+import { ListingEventModel } from '../src/infraestructure/db/mongo/models/listing-event.model';
+import { ListingModel } from '../src/infraestructure/db/mongo/models/listing.model';
+import { PriceHistoryModel } from '../src/infraestructure/db/mongo/models/price-history.model';
+import { ProductModel } from '../src/infraestructure/db/mongo/models/product.model';
+import { ProfileModel } from '../src/infraestructure/db/mongo/models/profile.model';
+import { QueryLogModel } from '../src/infraestructure/db/mongo/models/query-log.model';
+import { SearchDocumentModel } from '../src/infraestructure/db/mongo/models/search-document.model';
+import { ServiceTaxonomyModel } from '../src/infraestructure/db/mongo/models/service-taxonomy.model';
+import { SynonymModel } from '../src/infraestructure/db/mongo/models/synonym.model';
 import { UserModel } from '../src/infraestructure/db/mongo/models/user.model';
 import { MongooseDatabase } from './setup-db';
 
@@ -14,5 +26,17 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await UserModel.deleteMany({});
+  await ProfileModel.deleteMany({});
+  await CategoryModel.deleteMany({});
+  await ServiceTaxonomyModel.deleteMany({});
+  await CategoryAttributeSchemaModel.deleteMany({});
+  await ProductModel.deleteMany({});
+  await PriceHistoryModel.deleteMany({});
+  await ListingModel.deleteMany({});
+  await ListingEventModel.deleteMany({});
+  await SearchDocumentModel.deleteMany({});
+  await SynonymModel.deleteMany({});
+  await QueryLogModel.deleteMany({});
+  await FavoriteModel.deleteMany({});
   await dbInstance?.close();
 });

3630840dd38940e7924f29ab570371bfcbe4be9f
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-07 14:35:39 -0300

    feat(mongo): add user mongoose schema

--- src/infraestructure/db/mongo/schema/user.schema.ts
+++ src/infraestructure/db/mongo/schema/user.schema.ts
@@ -1,26 +1,32 @@
 import { Schema } from 'mongoose';
+import { EUserStatus } from '../../../../domain/identity/entity/enums/EUserStatus';
 import type { IMUser } from '../models/user.model';
 
 export const UserSchema = new Schema<IMUser>(
   {
-    id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
+    id: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
     email: {
       type: String,
       required: true,
       unique: true,
+      index: true,
       match: [
         /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
         'Please provide a valid email address',
       ],
     },
+    phone: { type: String, required: true },
+    cpf: { type: String, required: true, unique: true, index: true },
+    birthDate: { type: String, required: true },
+    verified: { type: Boolean, required: true, default: false },
+    phoneVerified: { type: Boolean, required: true, default: false },
+    status: {
+      type: String,
+      enum: Object.values(EUserStatus),
+      required: true,
+      default: EUserStatus.PENDING_VERIFICATION,
+    },
   },
-  { timestamps: true },
+  { timestamps: true, collection: 'users' },
 );

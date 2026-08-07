cf4429e57676430934cbd5287cb781224980e2d6
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-07 14:35:46 -0300

    test(user): add integration test create-user.int.test.ts

--- src/__tests__/integration/user/controller/create-user.int.test.ts
+++ src/__tests__/integration/user/controller/create-user.int.test.ts
@@ -3,13 +3,13 @@ import { app } from '../../../../../jest/setup-integration-tests';
 import { UserModel } from '../../../../infraestructure/db/mongo/models/user.model';
 import { validUserMock } from '../../../__mocks__/user.mock';
 
-const paramsCreate = validUserMock({
-  name: 'Whitebeard',
-  email: 'whitebeard@email.com',
-});
-
-describe('When we try to create a valid user', () => {
+describe('when we create a valid user via HTTP', () => {
   it('should return success and the created user', async () => {
+    const paramsCreate = validUserMock({
+      fullName: 'Whitebeard',
+      email: `whitebeard+${Date.now()}@email.com`,
+    });
+
     const { body, statusCode } = await supertest(app.app)
       .post(`/users`)
       .send(paramsCreate);
@@ -18,15 +18,16 @@ describe('When we try to create a valid user', () => {
 
     expect(body).toMatchObject({
       id: paramsCreate.id,
-      name: paramsCreate.name,
+      fullName: paramsCreate.fullName,
       email: paramsCreate.email,
+      cpf: paramsCreate.cpf,
     });
     expect(body.createdAt).toBeDefined();
     expect(new Date(body.createdAt).toISOString()).toBe(body.createdAt);
     expect(statusCode).toBe(201);
     expect(userInDb).toMatchObject({
       id: paramsCreate.id,
-      name: paramsCreate.name,
+      fullName: paramsCreate.fullName,
       email: paramsCreate.email,
     });
   });

97ca66b53c6341a19c08df3765b5d8275e312284
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-07 14:35:42 -0300

    feat(configuration): add product.service.factory.ts

---
+++ src/configuration/factory/product.service.factory.ts
@@ -0,0 +1,18 @@
+import { ProductService } from '../../domain/catalog/service/product.service';
+import { CategoryRepositoryRead } from '../../infraestructure/repository/catalog/category.repository.read';
+import { PriceHistoryRepositoryWrite } from '../../infraestructure/repository/catalog/price-history.repository.write';
+import { ProductRepositoryRead } from '../../infraestructure/repository/catalog/product.repository.read';
+import { ProductRepositoryWrite } from '../../infraestructure/repository/catalog/product.repository.write';
+import { EventPublisherFactory } from './messaging/event-publisher.factory';
+
+export class ProductServiceFactory {
+  static create() {
+    return new ProductService({
+      productRepositoryRead: new ProductRepositoryRead(),
+      productRepositoryWrite: new ProductRepositoryWrite(),
+      categoryRepositoryRead: new CategoryRepositoryRead(),
+      priceHistoryRepositoryWrite: new PriceHistoryRepositoryWrite(),
+      eventPublisher: EventPublisherFactory.create(),
+    });
+  }
+}

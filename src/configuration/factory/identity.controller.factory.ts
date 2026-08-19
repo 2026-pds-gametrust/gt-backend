9720d8085dc3524b5476225c386f228b29f654a4
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-19 20:47:36 -0300

    feat(configuration): update identity.controller.factory.ts

--- src/configuration/factory/identity.controller.factory.ts
+++ src/configuration/factory/identity.controller.factory.ts
@@ -1,5 +1,7 @@
 import { IdentityController } from '../../application/controllers/identity.controller';
 import { IController } from '../../domain/server/interfaces/IController';
+import { AuthServiceFactory } from './auth.service.factory';
+import { CepServiceFactory } from './cep.service.factory';
 import { ProfileServiceFactory } from './profile.service.factory';
 import { UserServiceFactory } from './user.service.factory';
 
@@ -8,6 +10,8 @@ export class IdentityControllerFactory {
     return new IdentityController(
       UserServiceFactory.create(),
       ProfileServiceFactory.create(),
+      AuthServiceFactory.create(),
+      CepServiceFactory.create(),
     );
   }
 }

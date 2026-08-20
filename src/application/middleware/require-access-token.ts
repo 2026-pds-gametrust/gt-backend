57d046945566007fc65f5c9b0e81d101908cef13
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-20 10:34:52 -0300

    feat(identity): update require access token

--- src/application/middleware/require-access-token.ts
+++ src/application/middleware/require-access-token.ts
@@ -1,10 +1,13 @@
 import { handleTranslatedError } from '@sauvvitech/st-packages';
 import { NextFunction, Request, Response } from 'express';
 import { EErrorCode } from '../../domain/common/errors/enums/EErrorCode';
+import type { IActorContext } from '../../domain/common/types/actor-context';
 import { ErrorCatalog } from '../../infraestructure/i18n/error-catalog';
 
+type RequestWithActor = Request & { actor?: IActorContext };
+
 export function requireAccessToken(
-  req: Request,
+  req: RequestWithActor,
   res: Response,
   next: NextFunction,
 ): void {
